be6b720b095b86ba8ef76bc13494bf2aabe25d49
openwell <timileyinojo@yahoo.com>
2021-05-29 21:50:36 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# lomolink_kenet
---
+++ notes.js
@@ -0,0 +1,14 @@
+// Extra small devices (portrait phones, less than 576px)
+@media (max-width: 575.98px) { ... }
+
+// Small devices (landscape phones, less than 768px)
+@media (max-width: 767.98px) { ... }
+
+// Medium devices (tablets, less than 992px)
+@media (max-width: 991.98px) { ... }
+
+// Large devices (desktops, less than 1200px)
+@media (max-width: 1199.98px) { ... }
+
+[2:39 PM, 5/29/2021] Uwakwe E: https://instagram.com/lomolink_consult?utm_medium=copy_link
+[2:40 PM, 5/29/2021] Uwakwe E: https://www.facebook.com/LomoLink/
